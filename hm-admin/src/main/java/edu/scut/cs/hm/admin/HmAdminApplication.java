c1294638be0534f6f9d772cfce97f67eb1ceae4f
ZeTao Wang <wzt3309@gmail.com>
2018-04-12 20:59:18 +0800

    添加pom与配置文件

---
+++ hm-admin/src/main/java/edu/scut/cs/hm/admin/HmAdminApplication.java
@@ -0,0 +1,12 @@
+package edu.scut.cs.hm.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmAdminApplication.class, args);
+    }
+}
